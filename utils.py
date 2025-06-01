6db8edd5c1e9cf3929d93bcd6a72ad388e124588
Kuba <kubagolec21@gmail.com>
2025-06-01 15:54:41 +0200

    Teraz bedzie git

--- utils.py
+++ utils.py
@@ -3,17 +3,21 @@ Moduł utils zawiera podstawowe funkcje kalkulatora: dodawanie, odejmowanie,
 mnożenie i dzielenie dwóch liczb całkowitych.
 """
 
+
 def add(a: int, b: int) -> int:
     """Zwraca sumę dwóch liczb całkowitych a i b."""
     return a + b
+
 
 def subtract(a: int, b: int) -> int:
     """Zwraca różnicę dwóch liczb całkowitych a i b."""
     return a - b
 
+
 def multiply(a: int, b: int) -> int:
     """Zwraca iloczyn dwóch liczb całkowitych a i b."""
     return a * b
+
 
 def divide(a: int, b: int) -> float:
     """Zwraca iloraz dwóch liczb całkowitych a i b jako liczbę zmiennoprzecinkową."""
