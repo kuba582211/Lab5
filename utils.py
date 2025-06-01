ab1e237fc460df9885adec3b2efc44912e481ca6
Kuba <kubagolec21@gmail.com>
2025-06-01 15:52:31 +0200

    Dodano kalkulator z CI/CD

--- utils.py
+++ utils.py
@@ -1,9 +1,20 @@
-# Exemplary calculator functions
-def add (a: int , b: int) -> int :
+"""
+Moduł utils zawiera podstawowe funkcje kalkulatora: dodawanie, odejmowanie,
+mnożenie i dzielenie dwóch liczb całkowitych.
+"""
+
+def add(a: int, b: int) -> int:
+    """Zwraca sumę dwóch liczb całkowitych a i b."""
     return a + b
-def subtract (a: int , b: int) -> int :
+
+def subtract(a: int, b: int) -> int:
+    """Zwraca różnicę dwóch liczb całkowitych a i b."""
     return a - b
-def multiply (a: int , b: int) -> int :
+
+def multiply(a: int, b: int) -> int:
+    """Zwraca iloczyn dwóch liczb całkowitych a i b."""
     return a * b
-def divide ( a: int , b : int ) -> float :
+
+def divide(a: int, b: int) -> float:
+    """Zwraca iloraz dwóch liczb całkowitych a i b jako liczbę zmiennoprzecinkową."""
     return a / b
