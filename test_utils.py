acb41c69d435e0e870618a8cd2342990a3836c0c
Kuba <kubagolec21@gmail.com>
2025-06-04 15:56:59 +0200

    Add to_binary function with validation and tests

--- test_utils.py
+++ test_utils.py
@@ -1,24 +1,24 @@
-# test_utils.py
-
 import pytest
-import utils
+from utils import to_binary
 
+# Test poprawnej konwersji
+@pytest.mark.parametrize("num, expected", [
+    (0, '0b0'),
+    (1, '0b1'),
+    (2, '0b10'),
+    (100, '0b1100100'),
+])
+def test_valid_conversion(num, expected):
+    assert to_binary(num) == expected
 
-@pytest.mark.parametrize("a, b, expected", [(1, 2, 3), (2, 3, 5), (3, 4, 7)])
-def test_add(a, b, expected):
-    assert utils.add(a, b) == expected
+# Test liczby poza zakresem
+@pytest.mark.parametrize("num", [-1, 101, 999])
+def test_out_of_range(num):
+    with pytest.raises(ValueError, match="Number out of allowed range"):
+        to_binary(num)
 
-
-@pytest.mark.parametrize("a, b, expected", [(2, 1, 1), (5, 3, 2)])
-def test_subtract(a, b, expected):
-    assert utils.subtract(a, b) == expected
-
-
-@pytest.mark.parametrize("a, b, expected", [(2, 3, 6), (4, 5, 20)])
-def test_multiply(a, b, expected):
-    assert utils.multiply(a, b) == expected
-
-
-@pytest.mark.parametrize("a, b, expected", [(4, 2, 2.0), (9, 3, 3.0)])
-def test_divide(a, b, expected):
-    assert utils.divide(a, b) == expected
+# Test liczby nienaturalnej
+@pytest.mark.parametrize("num", [1.5, 2.7, "10", None])
+def test_invalid_type(num):
+    with pytest.raises(TypeError, match="Number must be a natural integer"):
+        to_binary(num)
